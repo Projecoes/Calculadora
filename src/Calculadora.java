2ab7e5aaa54fb2269aa5099c078dab89b02ceb9a
Projecoes <andrewcoelho2507@gmail.com>
2024-12-09 18:33:09 -0300

    removendo a Interface Texto adicionando o método texto na própria classe Calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,10 +1,17 @@
 import java.util.Scanner;
 
-public class Calculadora extends Calculos implements Texto {
+public class Calculadora extends Calculos {
     Scanner sc = new Scanner(System.in);
 
     private int opcao;
 
+    public void texto() {
+        System.out.println("Digite o primeiro número:");
+        setPrimeiroNumero(sc.nextInt());
+
+        System.out.println("Digite o segundo número:");
+        setSegundoNumero(sc.nextInt());
+    }
 
     public void exibeMenu() {
         do {
@@ -19,33 +26,37 @@ public class Calculadora extends Calculos implements Texto {
                     0 - Sair
                     *****************************
                     """);
+
             opcao = sc.nextInt();
-
-
             switch (opcao) {
                 case 1:
                     texto();
                     System.out.println(getPrimeiroNumero() + " + " + getSegundoNumero() + " = " + Soma());
                     break;
+
                 case 2:
                     texto();
                     System.out.println(getPrimeiroNumero() + " - " + getSegundoNumero() + " = " + Subtracao());
                     break;
+
                 case 3:
                     texto();
                     System.out.println(getPrimeiroNumero() + " * " + getSegundoNumero() + " = " + multiplicacao());
                     break;
+
                 case 4:
                     texto();
                     if (getSegundoNumero() == 0) {
                         System.out.println("O segundo Numero nao pode ser zero");
-                    } else{
+                    } else {
                         System.out.println(getPrimeiroNumero() + " / " + getSegundoNumero() + " = " + divisao());
                     }
                     break;
+
                 case 0:
                     System.out.println("Saindo...");
                     break;
+
                 default:
                     System.out.println("Opção incorreta");
                     break;
@@ -54,15 +65,5 @@ public class Calculadora extends Calculos implements Texto {
         } while (opcao != 0);
 
     }
-
-    @Override
-    public void texto() {
-
-            System.out.println("Digite o primeiro número:");
-            setPrimeiroNumero(sc.nextInt());
-
-            System.out.println("Digite o segundo número:");
-            setSegundoNumero(sc.nextInt());
-        }
 }
 
